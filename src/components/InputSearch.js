013624d3aa0908ae7fee1fd3be7dc8d0997af411
Aureen <yukiansff@gmail.com>
2020-11-01 22:10:45 +0100

    Used Algolia places to create an input for cities

---
+++ src/components/InputSearch.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import AlgoliaPlaces from 'algolia-places-react';
+
+function InputSearch() {
+
+  const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
+  const [lat, setLat] = useState('')
+  const [lng, setLng] = useState('')
+
+  return (
+    <AlgoliaPlaces
+      placeholder='Write an address here'
+ 
+      options={{
+        appId: 'plCIL43TO1Z4',
+        apiKey: 'be563ce995a9d539bed17e7dd31bb4f3',
+        type: 'city'
+      }}
+ 
+      onChange={({ query, rawAnswer, suggestion, suggestionIndex }) => {
+        setCity(suggestion.name)
+        setCountry(suggestion.country)
+        setLat(suggestion.latlng.lat)
+        setLng(suggestion.latlng.lng)
+      }}
+    />
+  )
+}
+
+export default InputSearch

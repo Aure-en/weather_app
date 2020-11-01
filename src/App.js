40c45a5341b52a46e63265b91ad56ce33934812e
Aureen <yukiansff@gmail.com>
2020-11-01 21:40:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import InputSearch from './components/InputSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <InputSearch />
+    </div>
+  );
+}
+
+export default App;
